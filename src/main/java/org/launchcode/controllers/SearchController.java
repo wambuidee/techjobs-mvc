e433c3fee2a00307ef3d9038c369aa1e85e05c2e
Dorothy Mwangi <dmwangiw@gmail.com>
2019-04-01 12:36:20 -0500

    Techjobs-MVC Assignment - Completed TODO's

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -24,4 +24,24 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
+    @RequestMapping(value = "results")
+    public String searchResults(Model model, @RequestParam String searchType,@RequestParam String searchTerm ){
+
+        ArrayList<HashMap<String, String>> jobs;
+        if(searchType.equals("all")|| searchTerm.equals("")) {
+            jobs = JobData.findByValue(searchTerm);
+
+        }else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("searchType", searchType);
+        return "search";
+
+    }
+
+
+
 }
